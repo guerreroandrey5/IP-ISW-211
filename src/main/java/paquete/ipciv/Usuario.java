6ed033e9bc57c65c4f0bd5cbb99516696ad90e8f
Cris <Cris@DESKTOP-QEEV68G>
2021-06-24 09:31:56 -0600

    scanner agregado

--- src/main/java/paquete/ipciv/Usuario.java
+++ src/main/java/paquete/ipciv/Usuario.java
@@ -4,11 +4,17 @@
  * and open the template in the editor.
  */
 package paquete.ipciv;
-
+import java.util.Scanner;
 /**
  *
  * @author Cris
  */
 public class Usuario {
+    public Scanner lee = new Scanner(System.in);  
+    private int ID;
+    private String Name;
+    
+    
+    
     
 }
